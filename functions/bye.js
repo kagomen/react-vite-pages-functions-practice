ab164d9150eb767e2d4b148c209cfe0894565b1b
kagome <fu.taz.1102@gmail.com>
2024-07-03 23:05:11 +0900

    add handle

--- functions/bye.js
+++ functions/bye.js
@@ -1,8 +1,10 @@
 import { Hono } from "hono"
+import { handle } from "hono/cloudflare-pages"
 
 const app = new Hono()
 
 app.get("/bye", (c) => {
   return c.text("bye, world")
 })
-export default app
+
+export const onRequest = handle(app)
